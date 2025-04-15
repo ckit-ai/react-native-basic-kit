0d65646c7ce7f26cffdfa14cdde3c948d73e5c1c
mrt <thiswallz@gmail.com>
2025-04-15 09:04:21 -0500

    chore: base gluestack

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Box } from "@/components/ui/box";
+import { Text } from "@/components/ui/text";
+
+function Home() {
+    return (
+        <Box
+            className="bg-primary-500 p-5"
+        >
+            <Text className='text-typography-0'>
+                This is the Box
+            </Text>
+        </Box>
+    );
+}
+
+export default Home;
